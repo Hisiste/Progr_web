97650ea4f9f8314f6c5b16ed15043b4514c8af95
Adriz <aditooliva@gmail.com>
2022-09-24 22:33:35 +0000

    Hecho 'Modelos y Migraciones' - Primer commit.

---
+++ database/migrations/2022_09_20_233455_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 250);
+            $table->text('address', 500)->nullable();
+            $table->string('phone', 10)->nullable();
+            $table->string('rfc', 20)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
